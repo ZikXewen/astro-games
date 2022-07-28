674c09966579e17d66ca94032946f6ab6e8a2b7a
ZikXewen <datasup12@gmail.com>
2022-07-28 14:55:15 +0700

    mswp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import wasm from 'vite-plugin-wasm'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
-});
+  integrations: [preact(), tailwind()],
+  vite: { plugins: [wasm()] },
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        emoji: [
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
   },
   plugins: [],
   safelist: ['bg-green-500', 'bg-yellow-500'],
