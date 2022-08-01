d3fc8bc5ca07857b1a1ee9999455e170842b98e6
ZikXewen <datasup12@gmail.com>
2022-08-01 14:50:32 +0700

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-import tailwind from '@astrojs/tailwind'
-import wasm from 'vite-plugin-wasm'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+import wasm from 'vite-plugin-wasm';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-  vite: { plugins: [wasm()], optimizeDeps: { exclude: ['mswp'] } },
-})
+  integrations: [preact(), tailwind(), svelte()],
+  vite: {
+    plugins: [wasm()],
+    optimizeDeps: {
+      exclude: ['mswp']
+    }
+  }
+});
